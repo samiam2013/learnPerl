52013a7d497f97aff8be8315c2b30595241f2b40
Sam <samiam2013@gmail.com>
2020-11-15 19:17:14 -0600

    fizz buzz

---
+++ Elementary/ex05_fizz_buzz_ex04.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+print STDOUT "number to fizzbuzz sum to: ";
+my $max_num = <STDIN>;
+my $sum = 0;
+eval {
+  for (1..$max_num){
+    if (($_ % 3 == 0) || ($_ % 5 == 0)) {
+      $sum += $_
+    }
+  }
+};
+if ($@) {
+  print STDERR "there was a problem trying to understand your number?\n";
+} else {
+  print STDOUT "your sum is $sum\n";
+}
+
+exit;
