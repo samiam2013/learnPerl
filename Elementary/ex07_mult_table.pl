a17f9bb79a93588d0ce012fdcc6b38daf6ec4fdf
Sam <samiam2013@gmail.com>
2020-11-15 20:02:44 -0600

    multiplication table

---
+++ Elementary/ex07_mult_table.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my ($product, $output);
+for my $x (1..24){
+  for my $y (1..24){
+    $product = $x * $y;
+    $output = $product < 10 ? "   " : ($product < 100 ? "  " : " ");
+    print $output." $product";
+  }
+  print "\n";
+}
