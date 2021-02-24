eff52ba7d39f6b4ccf29210858fb4bf1488e309f
Sam <samiam2013@gmail.com>
2021-02-23 22:33:33 -0600

    just instructions

---
+++ Lists_and_Strings/ex02_reverse_in_place.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Data::Dumper;
+
+# Write function that reverses a list, preferably in place.
+print STDERR "how do you handle in-place reversal in perl???\n";
+
+
+exit;
