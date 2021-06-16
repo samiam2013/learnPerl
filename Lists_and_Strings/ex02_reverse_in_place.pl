bb9c41a8842b08133516e5c55ced5754a54e77e9
Samuel Myres <samiam2013@gmail.com>
2021-06-16 00:00:08 -0500

    refs for subroutines make this.. routine

--- Lists_and_Strings/ex02_reverse_in_place.pl
+++ Lists_and_Strings/ex02_reverse_in_place.pl
@@ -4,7 +4,28 @@ use warnings;
 use Data::Dumper;
 
 # Write function that reverses a list, preferably in place.
-print STDERR "how do you handle in-place reversal in perl???\n";
 
+my @array = ('first', 'second', 'third', 'fourth', 'fifth');
+
+sub reverse_in_place {
+	my %args = @_;
+	my $array_ref = $args{'array'};
+	my $arr_len = scalar(@$array_ref)-1;
+	#print "made it in: arr_len: $arr_len\n";
+	#print Data::Dumper::Dumper($array_ref);
+	for my $i (0 .. ($arr_len)){
+		if ($i < ($arr_len/2)) {
+			my $buf = $array_ref->[$i];
+			$array_ref->[$i] = $array_ref->[$arr_len-$i];
+			$array_ref->[$arr_len-$i] = $buf;
+		}
+	}
+}
+
+reverse_in_place('array' => \@array);
+
+for my $i (0 .. (scalar(@array)-1)){
+	print "$array[$i]\n";
+}
 
 exit;
