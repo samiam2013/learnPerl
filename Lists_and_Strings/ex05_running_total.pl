6c362707b116330aa1a38e3a8b212990de4911d1
Samuel Myres <samiam2013@gmail.com>
2021-06-21 19:18:35 -0500

    running sum finished

--- Lists_and_Strings/ex05_running_total.pl
+++ Lists_and_Strings/ex05_running_total.pl
@@ -5,8 +5,20 @@ use Data::Dumper;
 
 # 5 write a function that computes the running total of a list
 
-print Data::Dumper::Dumper(['oneVal', 'twoVal']);
+sub r_total {
+	my %args = @_;
+	my $list = $args{'list'};
+	my @running_total = ();
+	my $i = 0;
+	foreach my $value (@{$list}){
+		push(@running_total, ($i == 0 ? $value : $running_total[$i-1] + $value));	
+		$i++;
+	}
+	return \@running_total;
+}
 
+my @list = ( 1, 3, 6, 7, 10 );
 
+print Data::Dumper::Dumper(r_total('list' => \@list));
 
 exit;
