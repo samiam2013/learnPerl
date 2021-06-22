718736cfe8903a32a07f0990318e53cc7b9beb91
Samuel Myres <samiam2013@gmail.com>
2021-06-21 19:10:29 -0500

    add instructions ex05

---
+++ Lists_and_Strings/ex05_running_total.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Data::Dumper;
+
+# 5 write a function that computes the running total of a list
+
+print Data::Dumper::Dumper(['oneVal', 'twoVal']);
+
+
+
+exit;
